82afcef98f4f78b16137214b411436f4af01ef6d
Savandi <savandikodithuwakku@gmail.com>
2025-03-09 09:48:07 +0530

    Navbar is updated

--- index.php
+++ index.php
@@ -26,27 +26,28 @@ $highestWicketTaker = mysqli_fetch_assoc($highestWicketTakerResult);
 </head>
 <body>
     <!-- Navigation Bar -->
-    <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #3498db;">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Spirit11 Admin Panel</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Admin Dashboard</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="admin_profile.html">Admin Profile</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="login.html">Logout</a>
-                    </li>
-                </ul>
-            </div>
+    <nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">Spiritx</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" 
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Admin Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="admin_profile.html">Admin Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link logout" href="login.html">Logout</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+    </div>
+</nav>
 
     <!-- Main Content -->
     <div class="content">
@@ -73,7 +74,7 @@ $highestWicketTaker = mysqli_fetch_assoc($highestWicketTakerResult);
                     <p id="topWicketTaker"><?php echo $highestWicketTaker['name']; ?></p>
                 </div>
             </div>
-            <h2>Players Management</h2>
+            <h1>Players Management</h1>
             <!-- Players Table -->
             <div class="players-management">
                 
