cd0cf91e4d48a85357f6a869f7fbbf7ccfd2cdc8
Savandi <savandikodithuwakku@gmail.com>
2025-03-09 09:36:40 +0530

    Forms updated

--- edit_player.php
+++ edit_player.php
@@ -37,6 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Player</title>
+    <link rel = "stylesheet" href = "styles_add_player.css">
 </head>
 <body>
     <h1>Edit Player</h1>
